2085d559fed029ee04d2f5903d075ea99bb651d5
wanbiguizhao <shiweishiweihubugui@live.cn>
2020-04-06 19:49:38 +0800

    feare/taskService 初始化后台处理任务

---
+++ taskService/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+    desc: 后台服务，用于处理任务。
+    author: liukun
+    date: 2020-04-04
+'''
+import pathlib
+import sys
+import os
+_project_root = str(pathlib.Path(__file__).resolve().parents[1])
+
+
+
+if __name__ == "__main__":
+    print("hello word")
